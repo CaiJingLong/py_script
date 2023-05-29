4c041d951253009631ffd1aaa0e44bdc499e8094
Caijinglong <cjl_spy@163.com>
2023-05-29 15:50:24 +0800

    Create extract_code.py

---
+++ tools/extract_code.py
@@ -0,0 +1,67 @@
+# 打开源码目录，提取出指定extension的文件，拼接到指定为止
+# 用于生成一个文件，应该是用于软著
+
+import os
+import sys
+
+
+def get_files(path: str, extenstions: list[str], exclude_names: list[str] = []):
+    """获取指定目录下所有指定后缀的文件"""
+
+    print("path:", path)
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        for filename in filenames:
+            if filename in exclude_names:
+                continue
+            for ext in extenstions:
+                if filename.endswith(ext):
+                    files.append(os.path.join(root, filename))
+    return files
+
+
+cwd = sys.path[0]
+
+files = get_files(f"{cwd}/..", [".java", ".kt"])
+dart_files = get_files(
+    f"{cwd}/..",
+    [".dart"],
+    [
+        "url.dart",
+        "http_utils.dart",
+        "main.dart",
+        "app.dart",
+    ],
+)
+
+files.extend(dart_files)
+
+print(f"共找到 {len(files)} 个文件")
+print(f"文件列表：{files}")
+
+result = f"{cwd}/../logs/result.txt"
+
+if os.path.exists(result):
+    os.remove(result)
+
+ignore_contents = [
+    "Generated file",
+    "void main()",
+    "Generate by",
+    "Automatically generated file",
+    "Copyright (C) 2016 The Android Open Source Project",
+]
+
+for file in files:
+    with open(file, "r") as f:
+        content = f.read()
+
+        if any([ignore_content in content for ignore_content in ignore_contents]):
+            continue
+
+        lines = content.split("\n")
+        content = "\n".join([line for line in lines if line.strip() != ""])
+        with open(result, "a+") as r:
+            r.write(f"{content}\n")
+
+print(f"提取完成，结果保存在 {result}")
